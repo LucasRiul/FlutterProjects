a59d845489dc661dd215be9bd5db03ffafbdb8f7
android <android@rpo-acad.unaerpnet.br>
2023-04-04 09:47:57 -0300

    first commit

---
+++ app07_tabbar/app0_tabbar/android/app/src/main/kotlin/com/example/app0_tabbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app0_tabbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app07_tabbar/app0_tabbar/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:app0_tabbar/view/principal_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TabBar',
+      home: PrincipalView(),
+    ),
+  );
+}
